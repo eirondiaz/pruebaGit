b8d5a80516d1cb7e9eb010b4a940851fd6f82b5e
oscarmora26 <oscarrander26>
2020-03-07 11:59:55 -0400

    Segundo commit oscar "evento al boton"

--- app/src/main/java/com/example/pruebagit/MainActivity.java
+++ app/src/main/java/com/example/pruebagit/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.pruebagit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +16,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.bottom);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "Funciona correctamente", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
