c9934d3e613c5720a24d3ab988eb29feaf12c0bc
oscarmora26 <oscarrander26>
2020-03-07 12:23:45 -0400

    Rama otro activity "Segundo_activity"

--- app/src/main/java/com/example/pruebagit/MainActivity.java
+++ app/src/main/java/com/example/pruebagit/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.pruebagit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,11 +18,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = findViewById(R.id.bottom);
+        btn = findViewById(R.id.btn);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getBaseContext(), "Funciona correctamente", Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(getBaseContext(), Segundo_activity.class);
+                startActivity(intent);
             }
         });
     }
---
+++ app/src/main/java/com/example/pruebagit/Segundo_activity.java
@@ -0,0 +1,14 @@
+package com.example.pruebagit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Segundo_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segundo_activity);
+    }
+}
